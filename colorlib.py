712109d9de195f1066fd80c46e8ff920da57db2f
AghnatHs <69133536+AghnatHs@users.noreply.github.com>
2020-08-19 07:42:06 +0700

    Add files via upload

---
+++ colorlib.py
@@ -0,0 +1,31 @@
+"""
+	Module contains a RGB color
+	Class Variable
+
+	rgb color from rapidtables.com
+"""
+
+class Color():
+	pass
+
+class Gray(Color):
+	gainsboro="#DCDCDC"
+	lightgray="#D3D3D3"
+	darkgray="#A9A9A9"
+	gray="#808080"
+	dimgray="#696969"
+	lightslategray="#778899"
+	slategray="#708090"
+	darkslategray="#2F4F4F"
+class White(Color):
+	white="FFFFFF"
+	snow="#FFFAFA"
+	honeydew="#F0FFF0"
+	mintcream="#F5FFFA"
+	azure="#F0FFFF"
+	aliceblue="#F0F8FF"
+	ghostwhite="#F8F8FF"
+	whitesmoke="#F5F5F5"
+	seashell="#FFF5EE"
+	beige="#F5F5DC"
+	oldlace="#FDF5E6"
